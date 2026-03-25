528f281e65cbf37ec24905b9c6fa99da7d0ae2f9
nirajan-chy <nirajanchaudhary00001@gmail.com>
2026-03-25 09:31:43 +0545

    feat:msg

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const connectMongo = require("./src/config/mongodb");
 const { port } = require("./src/config/env");
+const router = require("./src/routes/user.route");
 
 const app = express();
 app.use(express.json());
 connectMongo();
+app.use("/user", router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- server/src/config/env.js
+++ server/src/config/env.js
@@ -3,7 +3,7 @@ const { config } = require("dotenv");
 config();
 
 const MONGO = process.env.MONGO_URI;
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 module.exports = {
   MONGO,
